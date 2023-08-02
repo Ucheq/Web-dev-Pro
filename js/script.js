2b2f152edb1f143e6a152b303045067c1f95f1dc
Ucheq <vivylomani@gmail.com>
2023-08-02 03:26:14 +0100

    Fitness test

---
+++ js/script.js
@@ -0,0 +1,83 @@
+const productEffect = document.getElementById("watch1");
+
+productEffect.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const productEffect1 = document.getElementById("watch2");
+
+productEffect1.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const productEffect2 = document.getElementById("watch3");
+
+productEffect2.addEventListener("click", (event) => {
+    event.target.style.background = "darkgray";
+    event.target.style.color = "white";
+});
+
+const productEffect3 = document.getElementById("watch4");
+
+productEffect3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const prodEffect = document.getElementById("bag1");
+
+prodEffect.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const prodEffect1 = document.getElementById("bag2");
+
+prodEffect1.addEventListener("click", (event) => {
+    event.target.style.background = "lightblue";
+    event.target.style.color = "darkblue";
+});
+
+const prodEffect2 = document.getElementById("bag3");
+
+prodEffect2.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "orange";
+});
+
+const prodEffect3 = document.getElementById("bag4");
+
+prodEffect3.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "black";
+});
+
+const prodEffect4 = document.getElementById("foodflask");
+
+prodEffect4.addEventListener("click", (event) => {
+    event.target.style.background = "pink";
+    event.target.style.color = "darkblue";
+});
+
+const prodEffect5 = document.getElementById("foodflask1");
+
+prodEffect5.addEventListener("click", (event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "brown";
+});
+
+const prodEffect6 = document.getElementById("foodflask2");
+
+prodEffect6.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "lightblue";
+});
+
+const prodEffect7 = document.getElementById("Spoonset");
+
+prodEffect7.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "darkgrey";
+});
